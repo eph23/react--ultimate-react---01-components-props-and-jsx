f514a8d55dc9d9fb59e0377744693ce716649796
ephraim <ephraims.818@gmail.com>
2025-08-22 17:21:29 +0600

    REACT--ULTIMATE-REACT---COMPONENTS-PROPS-JSX - 04. Styling React Applications

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
     {
@@ -48,7 +49,7 @@ const pizzaData = [
 
 function App() {
     return (
-        <div>
+        <div className="container">
             <Header />
             <Menu />
             <Footer />
@@ -57,37 +58,37 @@ function App() {
 }
 
 function Header() {
-    return <h1>First React Pizza Co.</h1>;
+    return (
+        <header className="header">
+            <h1>First React Pizza Co.</h1>;
+        </header>
+    );
 }
 function Menu() {
     return (
-        <div>
+        <main className="menu">
             <h2>Our Menu</h2>
             <Pizza />
             <Pizza />
             <Pizza />
             <Pizza />
-        </div>
+        </main>
     );
 }
 
 function Footer() {
-    const hour = new Date().getHours();
-    console.log(hour);
-    const openHour = 12;
-    const closeHour = 22;
-    const isOpen = hour >= openHour && hour <= closeHour;
-    console.log(isOpen);
     return (
-        <footer>{new Date().toLocaleTimeString()} We're currently open</footer>
+        <footer className="footer">
+            {new Date().toLocaleTimeString()} We're currently open
+        </footer>
     );
 }
 
 function Pizza() {
     return (
         <div>
-            <img src="/pizzas/prosciutto.jpg" alt="Prosciutto" />
-            <h2>Pizza Prosciutto</h2>
+            <img src="pizzas/prosciutto.jpg" alt="Prosciutto" />
+            <h3>Pizza Prosciutto</h3>
             <p>Tomato, mozarella, ham, aragula, and burrata cheese</p>
         </div>
     );
