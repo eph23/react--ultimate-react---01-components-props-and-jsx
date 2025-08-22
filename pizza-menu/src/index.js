9d78ad3a40e11261bea9de6b9cf8a24f47dba9f6
ephraim <ephraims.818@gmail.com>
2025-08-22 17:51:29 +0600

    REACT--ULTIMATE-REACT---COMPONENTS-PROPS-JSX - 05. Passing and Receiving Props

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -68,10 +68,18 @@ function Menu() {
     return (
         <main className="menu">
             <h2>Our Menu</h2>
-            <Pizza />
-            <Pizza />
-            <Pizza />
-            <Pizza />
+            <Pizza
+                name="Pizza Spinaci"
+                ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
+                photoName="pizzas/prosciutto.jpg"
+                price={10}
+            />
+            <Pizza
+                name="Pizza Spinaci"
+                ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
+                photoName="pizzas/prosciutto.jpg"
+                price={12}
+            />
         </main>
     );
 }
@@ -84,12 +92,17 @@ function Footer() {
     );
 }
 
-function Pizza() {
+function Pizza(props) {
+    console.log(props);
+
     return (
-        <div>
-            <img src="pizzas/prosciutto.jpg" alt="Prosciutto" />
-            <h3>Pizza Prosciutto</h3>
-            <p>Tomato, mozarella, ham, aragula, and burrata cheese</p>
+        <div className="pizza">
+            <img src={props.photoName} alt={props.name} />
+            <div>
+                <h3>{props.name}</h3>
+                <p>{props.ingredients}</p>
+                <span>{props.price}</span>
+            </div>
         </div>
     );
 }
