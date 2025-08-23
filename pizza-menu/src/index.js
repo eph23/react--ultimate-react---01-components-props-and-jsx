d221bebf638bb2e5056d7db67466d2d6a1b79c0d
ephraim <ephraims.818@gmail.com>
2025-08-23 16:56:22 +0600

    REACT--ULTIMATE-REACT---COMPONENTS-PROPS-JSX - 06. Rendering Lists

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -64,22 +64,16 @@ function Header() {
         </header>
     );
 }
+
 function Menu() {
     return (
         <main className="menu">
             <h2>Our Menu</h2>
-            <Pizza
-                name="Pizza Spinaci"
-                ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
-                photoName="pizzas/prosciutto.jpg"
-                price={10}
-            />
-            <Pizza
-                name="Pizza Spinaci"
-                ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
-                photoName="pizzas/prosciutto.jpg"
-                price={12}
-            />
+            <ul className="pizzas">
+                {pizzaData.map((pizza) => {
+                    return <Pizza pizzaObj={pizza} key={pizza.name} />;
+                })}
+            </ul>
         </main>
     );
 }
@@ -96,14 +90,14 @@ function Pizza(props) {
     console.log(props);
 
     return (
-        <div className="pizza">
-            <img src={props.photoName} alt={props.name} />
+        <li className="pizza">
+            <img src={props.pizzaObj.photoName} alt={props.name} />
             <div>
-                <h3>{props.name}</h3>
-                <p>{props.ingredients}</p>
-                <span>{props.price}</span>
+                <h3>{props.pizzaObj.name}</h3>
+                <p>{props.pizzaObj.ingredients}</p>
+                <span>{props.pizzaObj.price}</span>
             </div>
-        </div>
+        </li>
     );
 }
 
